61172de0ebc2d9f81f745a0088c72423ad3e0983
MuhammadFaishal21 <muhammadfaishal325@gmail.com>
2023-11-09 21:59:16 +0800

    menambahkan surat ajuan dan template surat

--- application/views/admin/Template_user/sidebar.php
+++ application/views/admin/Template_user/sidebar.php
@@ -18,7 +18,7 @@
                         <nav class="sb-sidenav-menu-nested nav">
                             <a class="nav-link" href="<?php echo site_url('admin/user');?>">User</a>
                             <a class="nav-link" href="<?php echo site_url('admin/surat_masuk');?>">surat_masuk</a>
-                            <a class="nav-link" href="<?php echo site_url('admin/surat_ajuan');?>">surat_masuk</a>
+                            <a class="nav-link" href="<?php echo site_url('admin/surat_ajuan');?>">surat_ajuan</a>
                             <a class="nav-link" href="layout-sidenav-light.html">Disposisi</a>
                         </nav>
                     </div>
